4ab04b73ab00be3c5040650e48cc42590466cec2
Avinash Kumar <pappukumar.work@gmail.com>
2024-04-21 23:39:51 +0530

    Update test case

--- execute_test_cases.py
+++ execute_test_cases.py
@@ -1,15 +1,27 @@
 from selenium import webdriver
+from behave import given, then
+from behave.runner import Runner
+from behave.configuration import Configuration
 
 def execute_test_cases(test_cases, url):
     options = webdriver.ChromeOptions()
     options.add_argument("--ignore-ssl-errors=yes")
     options.add_argument("--ssl-protocol=TLSv1.2")
     driver = webdriver.Chrome(options=options)
-    context = {'driver': driver, 'url': url}
+    context = {}
+    context['driver'] = driver
+    context['url'] = url
 
+    custom_config = Configuration()
+    custom_config.feature_dirs = [] 
+    behave_runner = Runner(config=custom_config)
+    behave_runner.feature_locations = [] 
+
+    behave_runner.context = context
     for test_case in test_cases:
+        compiled_test_case = compile(test_case, '<string>', 'exec')
         try:
-            exec(test_case, context)
+            exec(compiled_test_case, globals(), locals())
             print(f"Test case '{test_case.split()[1]}' passed.")
         except AssertionError as e:
             print(f"Test case '{test_case.split()[1]}' failed: {e}")
--- generate_bdd_test_cases.py
+++ generate_bdd_test_cases.py
@@ -1,10 +1,19 @@
+from behave import given, when, then
 from jinja2 import Template
 
 template = Template("""
+@given('the website is accessible for {{ persona }} users')
 def test_accessibility_{{ persona }}(context):
-    context['driver'].get(context['url'])
-    assert context['driver'].find_element_by_tag_name('body').is_displayed() == True
+    pass
+
+@then('the website is accessible for {{ persona }} users')
+def check_accessibility_{{ persona }}(context):
+    assert context.driver.find_element_by_tag_name('body').is_displayed() == True
 """)
+
+@when('I navigate to the website')
+def navigate_to_website(context):
+    context.driver.get(context.url)
 
 def generate_bdd_test_cases(personas, url):
     test_cases = []
@@ -12,3 +21,4 @@ def generate_bdd_test_cases(personas, url):
         test_case = template.render(persona=persona, url=url)
         test_cases.append(test_case)
     return test_cases
+
